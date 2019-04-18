e8d7866f8e851c172588367238412450ccd31b5d
Prashant Karbhari <prkarb@microsoft.com>
2019-04-17 20:41:43 -0700

    inference code update & Readme enhancements

--- Project_One/inference_code/model_class.py
+++ Project_One/inference_code/model_class.py
@@ -20,6 +20,7 @@ class MyModel:
         root_path = "./assets/"
         with open(root_path + "x_scaler.pkl", "rb") as xfile:
             self.x_scaler = pickle.load(xfile)
+            print (self.x_scaler)
         with open(root_path + "y_scaler.pkl", "rb") as yfile:
             self.y_scaler = pickle.load(yfile)
         with open(root_path + "model.pkl", "rb") as mfile:
@@ -29,10 +30,14 @@ class MyModel:
         """
         input_package: json formatted string of the form
         {"age": integer, "hours-per-week" : double, "sex" : string, "occupation" string}
-
+        
         returns json formatted string of the form: {"estimated_wages" : float}
         """
+        #input_package = '{"age": 37, "hours-per-week" : 40.0, "sex" : "Female", "occupation" : "Exec-managerial"}'
+
+        
         x = transform_input(input_package)
+        print (x)
         x = self.x_scaler.transform(x)
         y = self.model.predict(x)
         y = self.y_scaler.inverse_transform(y)
